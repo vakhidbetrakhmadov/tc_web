d927727be87ac27e7853a10f37de1632d0801391
Vakhid Betrakhmadov <vakhid.dtt@gmail.com>
2019-05-29 21:14:38 +0300

    Refactoring

--- views.py
+++ views.py
@@ -84,10 +84,10 @@ def index_post():
 
      logfile_name = "/Tvm-tc/log.txt"
 
-     # command = base + program_part + size_part + autotuner_part + tuner_threads_part + tuner_generations_part + pop_size_part + number_of_elites_part + load_from_cache_part + store_to_cache_part + use_shared_mem_part  + unroll_copy_shared_part + use_read_only_cache_part + match_lib_calls_part + fix_params_part  + outer_schedule_fusion_strategy_part + intra_tile_fusion_strategy_part + map_to_blocks_part + map_to_threads_part + tile_part + unroll_part + "&> {}".format(logfile_name)
-     # print(command)
+     command = base + program_part + size_part + autotuner_part + tuner_threads_part + tuner_generations_part + pop_size_part + number_of_elites_part + load_from_cache_part + store_to_cache_part + use_shared_mem_part  + unroll_copy_shared_part + use_read_only_cache_part + match_lib_calls_part + fix_params_part  + outer_schedule_fusion_strategy_part + intra_tile_fusion_strategy_part + map_to_blocks_part + map_to_threads_part + tile_part + unroll_part + "&> {}".format(logfile_name)
+     print(command)
 
-     echo = "echo 'Hi' > {}".format(logfile_name)
+     echo = "echo {} > {}".format(command, logfile_name)
      print(echo)
 
      subprocess.call([echo], shell=True)
