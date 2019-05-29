4b3ee158118bb038f86474171ff26c03db6f7e6b
Vakhid Betrakhmadov <vakhid.dtt@gmail.com>
2019-05-29 21:20:51 +0300

    Refactoring

--- views.py
+++ views.py
@@ -82,7 +82,7 @@ def index_post():
      tile_part = "" if isNoneOrEmpty(tile) else '--tile {} '.format(tile)
      unroll_part = "" if isNoneOrEmpty(unroll) else '--unroll={} '.format(unroll)
 
-     logfile_name = "/Tvm-tc/log.txt"
+     logfile_name = "/log.txt"
 
      command = base + program_part + size_part + autotuner_part + tuner_threads_part + tuner_generations_part + pop_size_part + number_of_elites_part + load_from_cache_part + store_to_cache_part + use_shared_mem_part  + unroll_copy_shared_part + use_read_only_cache_part + match_lib_calls_part + fix_params_part  + outer_schedule_fusion_strategy_part + intra_tile_fusion_strategy_part + map_to_blocks_part + map_to_threads_part + tile_part + unroll_part + "&> {}".format(logfile_name)
      print(command)
