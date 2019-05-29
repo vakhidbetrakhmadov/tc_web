8018199f9211cd7ad7ab6850f5c1e56a015d769c
Vakhid Betrakhmadov <vakhid.dtt@gmail.com>
2019-05-29 20:18:53 +0300

    Refactoring

--- views.py
+++ views.py
@@ -84,11 +84,11 @@ def index_post():
 
      logfile_name = "/Tvm-tc/log.txt"
 
-     command = base + program_part + size_part + autotuner_part + tuner_threads_part + tuner_generations_part + pop_size_part + number_of_elites_part + load_from_cache_part + store_to_cache_part + use_shared_mem_part  + unroll_copy_shared_part + use_read_only_cache_part + match_lib_calls_part + fix_params_part  + outer_schedule_fusion_strategy_part + intra_tile_fusion_strategy_part + map_to_blocks_part + map_to_threads_part + tile_part + unroll_part + "&>> {}".format(logfile_name)
+     command = base + program_part + size_part + autotuner_part + tuner_threads_part + tuner_generations_part + pop_size_part + number_of_elites_part + load_from_cache_part + store_to_cache_part + use_shared_mem_part  + unroll_copy_shared_part + use_read_only_cache_part + match_lib_calls_part + fix_params_part  + outer_schedule_fusion_strategy_part + intra_tile_fusion_strategy_part + map_to_blocks_part + map_to_threads_part + tile_part + unroll_part + "&> {}".format(logfile_name)
 
      print(command)
 
-     os.system("echo "" > {}".format(logfile_name))
+     os.system("echo '' > {}".format(logfile_name))
      os.system(command)
      output = parser.parse(logfile_name)
 
